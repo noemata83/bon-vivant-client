78533cb0c5a8bdbc8d93b829e4922ce644c0ab2e
Tucker McKinney <noemata83@gmail.com>
2019-08-11 23:51:26 -0400

    Add logo to header

--- layouts/header/header.js
+++ layouts/header/header.js
@@ -1,23 +1,34 @@
-import styled from 'styled-components';
-import Container from '../../components/layout/container';
+import styled from 'styled-components'
+import Container from '../../components/layout/container'
+import Logo from './logo'
+import Link from 'next/link'
 
-export default (props) => (
-  <Header>
-    <Container>
-    <Title>Bon Vivant Cocktails</Title>
-    </Container>
-  </Header>
-);
+export default props => (
+	<Header>
+		<Container>
+			<TitleBox>
+				<Link href="/"><a><Logo /></a></Link>
+				<Link href="/"><Title>Bon Vivant Cocktails</Title></Link>
+			</TitleBox>
+		</Container>
+	</Header>
+)
+
+const TitleBox = styled.div`
+	display: flex;
+	width: 100%;
+	align-items: flex-end;
+`
 
 const Header = styled.header`
-  background-color: ${({theme}) => theme.color.primary};
-  color: #fff;
-  padding: 1rem 2rem 2rem;
-  box-shadow: 2px 2px 8px rgba(0,0,0,0.4), 4px 0px 10px rgba(0,0,0,0.1);
-  letter-spacing: 0.5ch;
-`;
+	background-color: ${({ theme }) => theme.color.primary};
+	color: #fff;
+	padding: 1rem 2rem 2rem;
+	box-shadow: 2px 2px 8px rgba(0, 0, 0, 0.4), 4px 0px 10px rgba(0, 0, 0, 0.1);
+	letter-spacing: 0.5ch;
+`
 
 const Title = styled.h1`
-  padding: 0;
-  margin: 0;
-`;
+	padding: 0;
+	margin: 0;
+`
